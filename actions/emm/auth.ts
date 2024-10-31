b52ce4a02e242e8750dff7dbfdb540cc631c4bed
tk20211228 <t3kuboki@gmail.com>
2024-10-31 16:57:57 +0900

    feat: Enhance Google authentication handling for production and development environments

--- actions/emm/auth.ts
+++ actions/emm/auth.ts
@@ -1,10 +1,19 @@
 import { google, Auth } from "googleapis";
 
 export function googleServiceAuth() {
-  // const auth = new google.auth.GoogleAuth({
-  const auth: Auth.GoogleAuth = new google.auth.GoogleAuth({
-    keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
-    scopes: ["https://www.googleapis.com/auth/androidmanagement"],
-  });
-  return auth;
+  if (process.env.NODE_ENV === "production") {
+    const auth: Auth.GoogleAuth = new google.auth.GoogleAuth({
+      credentials: JSON.parse(
+        process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON || "{}"
+      ),
+      scopes: ["https://www.googleapis.com/auth/androidmanagement"],
+    });
+    return auth;
+  } else {
+    const auth: Auth.GoogleAuth = new google.auth.GoogleAuth({
+      keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+      scopes: ["https://www.googleapis.com/auth/androidmanagement"],
+    });
+    return auth;
+  }
 }
