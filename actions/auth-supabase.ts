aaa32bfe25645fb35d5d7222c2fefae6d84fd16e
tk20211228 <t3kuboki@gmail.com>
2024-12-19 14:11:57 +0900

    feat: signInWithEmailOrUsername関数を修正し、リダイレクト処理を各サインインメソッド内に統合

--- actions/auth-supabase.ts
+++ actions/auth-supabase.ts
@@ -129,9 +129,12 @@ export const signInWithEmailOrUsername = async (formData: SignIn) => {
   // メールアドレスかユーザー名かを判断
   const isEmail = formData.emailOrUserName.includes("@");
   isEmail
-    ? await signInWithEmail(safeParsedFormData.data)
-    : await signInWithUsername(safeParsedFormData.data);
-  redirect("/projects");
+    ? await signInWithEmail(safeParsedFormData.data).then(() => {
+        redirect("/projects");
+      })
+    : await signInWithUsername(safeParsedFormData.data).then(() => {
+        redirect("/projects");
+      });
 };
 
 export const resendSignUpOPT = async ({
